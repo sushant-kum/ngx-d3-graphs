cbeb7ceb036eb1c1d0b2ce1208243ed693ffe4b4
Sushant Kumar <sushant.kum96@gmail.com>
2020-02-23 22:12:48 +0530

    base of docs

--- projects/ngx-d3-graphs-doc/src/app/app.module.ts
+++ projects/ngx-d3-graphs-doc/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-
-import { StackedAreaModule } from 'ngx-d3-graphs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [StackedAreaModule, BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
