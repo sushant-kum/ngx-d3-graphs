589bcce74594751d458c49cd863d35855899fb7a
Sushant Kumar <sushant.kum96@gmail.com>
2020-02-26 21:39:21 +0530

    fine-tune header and footer responsiveness

--- projects/ngx-d3-graphs-doc/src/app/components/header/header.module.ts
+++ projects/ngx-d3-graphs-doc/src/app/components/header/header.module.ts
@@ -9,11 +9,11 @@ import { MatButtonModule } from '@angular/material/button';
 /* Fontawesome Imports */
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 /* Solid Icons */
-import {} from '@fortawesome/free-solid-svg-icons';
+import { faCode as fasCode } from '@fortawesome/free-solid-svg-icons';
 /* Regular Icons */
 import {} from '@fortawesome/free-regular-svg-icons';
 /* Brand Icons */
-import { faGithub as fabGithub } from '@fortawesome/free-brands-svg-icons';
+import {} from '@fortawesome/free-brands-svg-icons';
 
 import { HeaderComponent } from './header.component';
 
@@ -35,8 +35,8 @@ import { HeaderComponent } from './header.component';
 export class HeaderModule {
   constructor(fa_icon_library: FaIconLibrary) {
     // Include solid fa icons
+    fa_icon_library.addIcons(fasCode);
     // Include regular fa icons
     // Include brand fa icons
-    fa_icon_library.addIcons(fabGithub);
   }
 }
