d6ae084ac754e881300841f257532b89b42f6f3c
Sushant Kumar <sushant.kum96@gmail.com>
2020-03-08 20:48:13 +0530

    /graphs/evolution

--- projects/ngx-d3-graphs-doc/src/app/pages/root/graphs/evolution/evolution.component.ts
+++ projects/ngx-d3-graphs-doc/src/app/pages/root/graphs/evolution/evolution.component.ts
@@ -1,12 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+/* Import Services */
+import { TitleService } from '@doc/src/app/serices/title/title.service';
+
+/* Import Configs */
+import { GRAPH_PAGES } from '@doc/src/app/config/config';
+
+/* Import Data models */
+import { Page } from '@doc/src/app/data-models/page/page';
+
 @Component({
   selector: 'app-evolution',
   templateUrl: './evolution.component.html',
   styleUrls: ['./evolution.component.scss']
 })
 export class EvolutionComponent implements OnInit {
-  constructor() {}
+  readonly evolution_sub_pages: Page[] = [];
 
-  ngOnInit(): void {}
+  constructor(private _title_svc: TitleService) {}
+
+  ngOnInit(): void {
+    this._title_svc.setTitle('Evolution');
+
+    this._setEvolutionSubPages();
+  }
+
+  /**
+   * Set value of evolution_sub_pages
+   *
+   * @author Sushant Kumar <sushant.kum96@gmail.com>
+   * @private
+   * @memberof EvolutionComponent
+   */
+  private _setEvolutionSubPages(): void {
+    for (const graph_page of GRAPH_PAGES) {
+      if (graph_page.id === 'evolution') {
+        for (const sub_page of graph_page.items) {
+          this.evolution_sub_pages.push(sub_page);
+        }
+        break;
+      }
+    }
+  }
 }
--- projects/ngx-d3-graphs-doc/src/app/pages/root/graphs/evolution/evolution.module.ts
+++ projects/ngx-d3-graphs-doc/src/app/pages/root/graphs/evolution/evolution.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/* Angular Material imports */
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+/* Fontawesome Imports */
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+/* Solid Icons */
+import { faAngleRight as fasAngleRight, faBookOpen as fasBookOpen } from '@fortawesome/free-solid-svg-icons';
+/* Regular Icons */
+import {} from '@fortawesome/free-regular-svg-icons';
+/* Brand Icons */
+import {} from '@fortawesome/free-brands-svg-icons';
+
 import { EvolutionRoutingModule } from './evolution-routing.module';
 import { EvolutionComponent } from './evolution.component';
 
 @NgModule({
   declarations: [EvolutionComponent],
-  imports: [CommonModule, EvolutionRoutingModule]
+  imports: [
+    CommonModule,
+    EvolutionRoutingModule,
+    /* Angular Material */
+    MatButtonModule,
+    MatCardModule,
+    MatTooltipModule,
+
+    /* Fontawesome */
+    FontAwesomeModule
+  ]
 })
-export class EvolutionModule {}
+export class EvolutionModule {
+  constructor(fa_icon_library: FaIconLibrary) {
+    // Include solid fa icons
+    fa_icon_library.addIcons(fasAngleRight, fasBookOpen);
+    // Include regular fa icons
+    // Include brand fa icons
+  }
+}
